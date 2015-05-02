7c8f4dc000173a2819ad79d380839274e0ad9f43
zhuangqh <zhuangqhc@163.com>
2015-05-02 21:11:07 +0800

    easy

---
+++ medium/Maximum_Depth.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+  int maxDepth(TreeNode* root) {
+    if (root == NULL) return 0;
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    return 1 + max(left, right);
+  }
+};
