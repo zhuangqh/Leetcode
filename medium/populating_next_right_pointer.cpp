159cbcdfe261b8791931b445fc3c843b1a319af2
zhuangqh <zhuangqhc@163.com>
2015-07-28 14:53:06 +0800

    recursion

---
+++ medium/populating_next_right_pointer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct TreeLinkNode {
+  int val;
+  TreeLinkNode *left, *right, *next;
+  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+class Solution {
+public:
+  void connect(TreeLinkNode *root) {
+    if (root == NULL)
+      return;
+    if (root->left)
+      root->left->next = root->right;
+
+    if (root->right) {
+      root->right->next = NULL;
+      if (root->next) {
+        root->right->next = root->next->left;
+      }
+    }
+
+    connect(root->left);
+    connect(root->right);
+  }
+};
