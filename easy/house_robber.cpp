c57dde9e1f7449ccc74b91319c41a45588782201
zhuangqh <zhuangqhc@163.com>
2015-10-26 01:08:03 +0800

    dynamic programming

---
+++ easy/house_robber.cpp
@@ -0,0 +1,26 @@
+//
+// Created by z on 2015/10/26.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int n = nums.size();
+        vector<int> dp(n+1, 0);
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i <= n; ++i)
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1]);
+
+        return dp[n];
+    }
+};
