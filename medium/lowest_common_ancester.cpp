a151e5880d7b412d0a687c2cb87b15c3722eee07
zhuangqh <zhuangqhc@163.com>
2016-01-10 00:53:22 +0800

    cursive

--- medium/lowest_common_ancester.cpp
+++ medium/lowest_common_ancester.cpp
@@ -10,28 +10,14 @@ struct TreeNode {
 
 class Solution {
 public:
-  bool isAncestor(TreeNode* root, TreeNode* node) {
-    if (root == NULL) return false;
-    return node == root ||
-      isAncestor(root->left, node) || isAncestor(root->right, node);
-  }
-  bool isCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    return isAncestor(root, p) && isAncestor(root, q);
-  }
   TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    if (root == NULL) return NULL;
+    // return when one of them is found
+    if (!root || root == p || root == q) return root;
 
-    bool left = isCommonAncestor(root->left, p, q),
-      right = isCommonAncestor(root->right, p, q);
-
-    if (left && !right) {
-      return lowestCommonAncestor(root->left, p, q);
-    } else if (!left && right) {
-      return lowestCommonAncestor(root->right, p, q);
-    } else if (!left && !right) {
-      return root;
-    } else {
-      return NULL;
-    }
+    // a node include p, q or both in the left sub-tree
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    if (left && right) return root;
+    return left ? left : right;
   }
 };
