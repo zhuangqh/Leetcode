348fca709baffeb4e586d6a5957bb6f5a621e088
庄清惠 <zhuangqhc@163.com>
2016-08-26 00:14:29 +0800

    add utils for each problem

---
+++ utils/common.h
@@ -0,0 +1,13 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <cmath>
+
+#endif
